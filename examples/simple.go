bad801e6b1962702df8ee55771d3ed2838caa291
xander seren <xander@theactualschool.com>
2017-07-12 09:19:56 -0400

    Adds a simple example

---
+++ examples/simple.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	arena "github.com/xanderseren/arena-go"
+)
+
+func getChannel(w http.ResponseWriter, r *http.Request) {
+	arena := arena.NewClient()
+	channel, err := arena.GetChannel("60260")
+
+	if err != nil {
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s", err)
+	} else {
+		ej, _ := json.Marshal(channel)
+
+		// Write content-type, statuscode, payload
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s", ej)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", getChannel)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
