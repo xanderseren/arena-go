65c23a46a4a48239303cfe6037038f158ea923fd
xander seren <xander@theactualschool.com>
2017-07-12 22:10:23 -0400

    expands block struct

--- block.go
+++ block.go
@@ -3,12 +3,81 @@ package arena
 import "fmt"
 
 type Block struct {
-	client    *Client
-	ID        int64  `json:"id"`
-	Title     string `json:"title"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-	Slug      string `json:"slug"`
+	client          *Client
+	ID              int              `json:"id"`
+	Title           string           `json:"title"`
+	UpdatedAt       string           `json:"updated_at"`
+	CreatedAt       string           `json:"created_at"`
+	State           string           `json:"state"`
+	CommentCount    int              `json:"comment_count"`
+	GeneratedTitle  string           `json:"generated_title"`
+	ContentHtml     *string          `json:"content_html"`
+	DescriptionHtml *string          `json:"description_html"`
+	Visibility      string           `json:"visibility"`
+	Content         *string          `json:"content"`
+	Description     *string          `json:"description"`
+	Source          SourceStruct     `json:"source"`
+	Image           ImageStruct      `json:"image"`
+	Embed           EmbedStruct      `json:embed`
+	Attachment      AttachmentStruct `json:"attachment"`
+	Metadata        MetadataStruct   `json:"metadata"`
+	BaseClass       string           `json:"base_class"`
+	Class           string           `json:"class"`
+	User            UserStruct       `json:"user"`
+}
+
+type SourceStruct *struct {
+	Url      string  `json:"url"`
+	Title    *string `json:"title"`
+	Provider struct {
+		Name string `json:"name"`
+		Url  string `json:"url"`
+	} `json:"provider"`
+}
+
+type ImageStruct *struct {
+	Filename    string `json:"filename"`
+	ContentType string `json:"content_type"`
+	UpdatedAt   string `json:"updated_at"`
+	Thumb       struct {
+		Url string `json:"url"`
+	} `json:"thumb"`
+	Square struct {
+		Url string `json:"url"`
+	} `json:"square"`
+	Display struct {
+		Url string `json:"url"`
+	} `json:"display"`
+	Large struct {
+		Url string `json:"url"`
+	} `json:"large"`
+	Original struct {
+		Url             string `json:"url"`
+		FileSize        int    `json:"file_size"`
+		FileSizeDisplay string `json:"file_size:display"`
+	} `json:"original"`
+}
+
+type EmbedStruct *struct {
+	Url           *string `json:"url"`
+	Type          *string `json:"type"`
+	Title         *string `json:"title"`
+	AuthorName    *string `json:"author_name"`
+	AuthorUrl     *string `json:"author_url"`
+	SourceUrl     *string `json:"source_url"`
+	Thumbnail_url *string `json:"thumbnail_url"`
+	Width         int     `json:"width"`
+	Height        int     `json:"height"`
+	Html          string  `json:"html"`
+}
+
+type AttachmentStruct *struct {
+	FileName        string `json:"file_name"`
+	FileSize        int    `json:"file_size"`
+	FileSizeDisplay string `json:"file_size_display"`
+	ContentType     string `json:"content_type"`
+	Extension       string `json:"extension"`
+	Url             string `json:"url"`
 }
 
 // GetBlock returns a channel based on the ID
--- channel.go
+++ channel.go
@@ -4,27 +4,66 @@ import "fmt"
 
 type Channel struct {
 	client            *Client
-	ID                int    `json:"id"`
-	Title             string `json:"title"`
-	CreatedAt         string `json:"created_at"`
-	UpdatedAt         string `json:"updated_at"`
-	AddedToAt         string `json:"added_to_at"`
-	Published         bool   `json:"published"`
-	Open              bool   `json:"open"`
-	Collaboration     bool   `json:"collaboration"`
-	CollaboratorCount int    `json:"collaborator_count"`
-	Slug              string `json:"slug"`
-	Length            int    `json:"length"`
-	Kind              string `json:"kind"`
-	Status            string `json:"status"`
-	UserID            int    `json:"user_id"`
-	BaseClass         string `json:"base_class"`
-	Page              int    `json:"page"`
-	Per               int    `json:"per"`
-	FollowerCount     int    `json:"follower_count"`
+	ID                int                   `json:"id"`
+	Title             string                `json:"title"`
+	CreatedAt         string                `json:"created_at"`
+	UpdatedAt         string                `json:"updated_at"`
+	AddedToAt         string                `json:"added_to_at"`
+	Published         bool                  `json:"published"`
+	Open              bool                  `json:"open"`
+	Collaboration     bool                  `json:"collaboration"`
+	CollaboratorCount int                   `json:"collaborator_count"`
+	Slug              string                `json:"slug"`
+	Length            int                   `json:"length"`
+	Kind              string                `json:"kind"`
+	Status            string                `json:"status"`
+	UserID            int                   `json:"user_id"`
+	Contents          []ContentsStruct      `json:"contents"`
+	BaseClass         string                `json:"base_class"`
+	Page              int                   `json:"page"`
+	Per               int                   `json:"per"`
+	Collaborators     []CollaboratorsStruct `json:"collaborators"`
+	FollowerCount     int                   `json:"follower_count"`
+	ShareLink         *string               `json:"share_link"`
+	Metadata          MetadataStruct        `json:"metadata"`
+	ClassName         string                `json:"class_name"`
+	CanIndex          bool                  `json:"can_index"`
+	User              UserStruct            `json:"user"`
 }
 
-// GetChannel returns a channel based on the ID
+type ContentsStruct struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	Slug      string `json:"slug"`
+}
+
+type CollaboratorsStruct struct {
+	ID        int    `json:"id"`
+	Slug      string `json:"slug"`
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	FullName  string `json:"full_name"`
+	Avatar    string `json:"avatar"`
+}
+
+type UserStruct struct {
+	ID        int    `json:"id"`
+	Slug      string `json:"slug"`
+	Username  string `json:"username"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	FullName  string `json:"full_name"`
+	Avatar    string `json:"avatar"`
+}
+
+type MetadataStruct struct {
+	Description string `json:"description"`
+}
+
+// GetChannel returns a channel based on the ID or slug
 func (c *Client) GetChannel(channelID string, args Arguments) (channel *Channel, err error) {
 	path := fmt.Sprintf("channels/%s", channelID)
 	err = c.Get(path, args, &channel)
@@ -33,3 +72,13 @@ func (c *Client) GetChannel(channelID string, args Arguments) (channel *Channel,
 	}
 	return
 }
+
+// GetChannelContents returns a channels contents based on the ID r slug
+// func (c *Client) GetChannelContents(channelID string, args Arguments) (channel *Channel, err error) {
+// 	path := fmt.Sprintf("channels/%s", channelID, "/%scontents")
+// 	err = c.Get(path, args, &channel)
+// 	if channel != nil {
+// 		channel.client = c
+// 	}
+// 	return
+// }
